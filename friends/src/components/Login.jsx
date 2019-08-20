387890802d9e23839fffb2abd64d4e9740591402
Eli Sacks <bastlifa@gmail.com>
2019-08-20 18:01:29 -0400

    added Material-UI, some styling, styled-comps for other styling

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -1,15 +1,28 @@
 import React, {useState} from "react"
-import { withFormik, Form, Field } from 'formik'
-import * as Yup from 'yup'
-import axios from 'axios'
+// import { withFormik, Form, Field } from 'formik'
+// import * as Yup from 'yup'
+// import axios from 'axios'
 import Loader from 'react-loader-spinner'
 import { postLogin } from '../actions'
 import { useDispatch, useSelector } from 'react-redux'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    button: {
+        margin: theme.spacing(1),
+    },
+    input: {
+        display: 'none',
+    },
+}));
 
 const LoginForm = (props) =>
 {
     const dispatch = useDispatch()
     let state = useSelector(state => state)
+
+    const classes = useStyles();
 
     const [inputs, setInputs] = useState({username: '', password: ''})
 
@@ -49,10 +62,10 @@ const LoginForm = (props) =>
                         onChange={handleChange}
                     />
                 </div>
-                <button style={{width:"175px"}}>
+                <Button onClick={handleSubmit} variant="outlined" color="primary" style={{width:"175px"}}>
                     {state.isLoading ? <Loader type="TailSpin" color="#00BFFF" height={10} width={10} /> 
                     : 'Submit'}
-                </button>
+                </Button>
             </form>
         </>
     )
--- friends/src/components/NewFriendForm.jsx
+++ friends/src/components/NewFriendForm.jsx
@@ -1,12 +1,38 @@
 import React, {useState} from "react"
 import {useDispatch} from 'react-redux'
 import { postFriend } from '../actions'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import {Form} from './StyledComps'
+
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+    button: {
+        margin: theme.spacing(1),
+    },
+}));
 
 const NewFriendForm = () =>
 {
     const [inputs, setInputs] = useState({age: '', email: '', name: ''})
     const dispatch = useDispatch()
-
+    const classes = useStyles();
     const handleChange = event =>
     {
         setInputs({...inputs, [event.target.name]: event.target.value})
@@ -19,31 +45,47 @@ const NewFriendForm = () =>
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
             <span>
                 {"New Friend Form"}
                 <br/>
             </span> 
-            <label>
-                Name:
-                <br/>
-                <input type="text" name="name" placeholder="Name" value={inputs.name} onChange={handleChange} />
-            </label>
-            <br/>
-            <label>
-                Age:
-                <br/>
-                <input type="number" name="age" placeholder="Age" value={inputs.age} onChange={handleChange} />
-            </label>
-            <br/>
-            <label>
-                Email:
-                <br/>
-                <input type="text" name="email" placeholder="Email" value={inputs.email} onChange={handleChange} />
-            </label>
-            <br/>
-            <button>Submit Friend!</button>
-        </form>
+            <TextField
+                id="standard-name"
+                label="Name"
+                className={classes.textField}
+                value={inputs.name}
+                onChange={handleChange}
+                margin="normal"
+                name="name"
+            />
+            <TextField
+                id="standard-number"
+                label=" "
+                placeholder="Age"
+                value={inputs.age}
+                onChange={handleChange}
+                type="number"
+                className={classes.textField}
+                InputLabelProps={{
+                shrink: true,
+                }}
+                margin="normal"
+                name="age"
+            />
+            <TextField
+                id="standard-email-input"
+                label="Email"
+                className={classes.textField}
+                type="email"
+                name="email"
+                autoComplete="email"
+                margin="normal"
+            />
+            <Button onClick={handleSubmit} variant="outlined" color="primary" style={{width:"175px", margin:"20px auto"}}>
+                Submit Friend
+            </Button>
+        </Form>
     )
 }
 
